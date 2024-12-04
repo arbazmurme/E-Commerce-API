3f38947b198d10baa4660189af00e3335bf330a8
arbaj murme <86678721+arbazmurme@users.noreply.github.com>
2024-12-04 14:23:35 +0530

    Update app.js
    eeee

--- app.js
+++ app.js
@@ -14,5 +14,5 @@ app.use("/api/auth", authRoutes);
 app.use((req, res) => res.send("<h1>Hello World!</h1>"));
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.POR || https://e-commerce-api-two-vert.vercel.app/;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
