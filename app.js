4a7716977bb90b5116c98dea8a527e1de0f6048c
arbazmurme <arbazmurme@gmail.com>
2024-12-04 13:45:55 +0530

    second line

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use((req, res) => res.send("<h1>Hello World!</h1>"));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
